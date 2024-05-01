ec22863dc533592b8ec3cc16e7e16137d6e23bd6
NishigandhaGawas <129956160+NishigandhaGawas@users.noreply.github.com>
2024-05-01 19:56:42 +0530

    fix: config changes

--- src/theme.js
+++ src/theme.js
@@ -1,16 +1,18 @@
 
 import { extendTheme } from '@chakra-ui/react';
 
+const config = {
+  initialColorMode: "dark",
+}
+
 const theme = extendTheme({
+  
   fonts: {
     body: 'Poppins, sans-serif',
     heading: 'Poppins, sans-serif',
   },
   config
 });
-const config = {
-  initialColorMode: 'dark',
-  useSystemColorMode: false,
-}
+
 
 export default theme;
